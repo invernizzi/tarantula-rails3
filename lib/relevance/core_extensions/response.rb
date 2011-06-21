0015042ae63e2a2f6d7b1aa34fe4e9ec2f99ac06
Luca Invernizzi <invernizzi.l@gmail.com>
2011-06-21 15:24:26 -0700

    the html? method always returned the same value

--- lib/relevance/core_extensions/response.rb
+++ lib/relevance/core_extensions/response.rb
@@ -3,7 +3,7 @@ module Relevance::CoreExtensions::Response
   def html?                                           
     # some versions of Rails integration tests don't set content type
     # so we are treating nil as html. A better fix would be welcome here.
-    ((content_type =~ %r{^text/html}) != nil)  || content_type == nil
+    ((content_type.to_s =~ %r{^text/html.*}) != nil)  || content_type == nil
   end
 end
 
